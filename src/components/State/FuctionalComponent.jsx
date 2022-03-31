f89550d4db410e673afa859b88fd483c3484794d
주지홍 <ju@jihongzzang.local>
2022-03-31 18:21:08 +0900

    Add : Class convert to Functional

---
+++ src/components/State/FuctionalComponent.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+export const FuctionalComponent = () => {
+  const [date, setDate] = useState(() => {
+    return new Date();
+  }); //==>state Update
+
+  const tick = () => {
+    setDate(new Date());
+  }; //==>state Update
+
+  useEffect(() => {
+    const interval = setInterval(() => tick());
+
+    return () => {
+      clearInterval(interval);
+    }; //=> willUnmount
+  }, []); //=> didMount
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Composition from '../components/Props/Composition';
 import Extraction from '../components/Props/Exraction/Extraction';
+import ClassComponent from '../components/State/ClassComponent';
+import { FuctionalComponent } from '../components/State/FuctionalComponent';
 const Home = () => {
   return (
     <>
       <Composition />
       <Extraction />
+      <ClassComponent />
+      <FuctionalComponent />
     </>
   );
 };
