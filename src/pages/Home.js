375e5a91bb3d76eb0b28f492b4ed9d55b1cf4f38
주지홍 <ju@jihongzzang.local>
2022-03-31 18:45:18 +0900

    Add : Class Component LifeCycle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Composition from '../components/Props/Composition';
 import Extraction from '../components/Props/Exraction/Extraction';
 import ClassComponent from '../components/State/ClassComponent';
+import ClassComponent2 from '../components/LifeCycle/ClassComponent';
 import { FuctionalComponent } from '../components/State/FuctionalComponent';
 const Home = () => {
   return (
     <>
-      <Composition />
+      <ClassComponent2 />
+      {/* <Composition />
       <Extraction />
       <ClassComponent />
-      <FuctionalComponent />
+      <FuctionalComponent /> */}
     </>
   );
 };
