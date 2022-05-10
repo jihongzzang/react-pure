971001066d0be6cf830ee02806c1f98a019edcea
jihongzzang <jujihong2@gmail.com>
2022-05-10 17:00:50 +0900

    Add: 트러블 슈팅 관련 내용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import MatchPage from './match/refactored-with-hooks';
 // import Greeting from '../ClassSection/Greeting';
 // import Greeting2 from '../ClassSection/Greeting2';
 // import Greeting3 from '../ClassSection/Greeting3';
+// import MatchPage from './match/refactored-with-hooks';
 // import MatchPage from '../pages/match/incomplete';
 // import MatchPageContainer from './match/refactored-with-container';
+import Page from '../FinalSection/Page';
+// import PageRefactored from '../FinalSection/Page-Refactored';
 
 const Home = () => {
   return (
@@ -14,7 +16,9 @@ const Home = () => {
       {/* <Greeting3 /> */}
       {/* <MatchPage /> */}
       {/* <MatchPageContainer /> */}
-      <MatchPage />
+      {/* <MatchPage /> */}
+      <Page />
+      {/* <PageRefactored /> */}
     </>
   );
 };
